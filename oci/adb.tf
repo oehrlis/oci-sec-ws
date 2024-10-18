2babb033815a1bfab188f19d6c5df27f4a8a1f9d
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-10-18 11:03:36 +0200

    add adb resource config

---
+++ oci/adb.tf
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------------------
+# OraDBA - Oracle Database Infrastructure and Security, 5630 Muri, Switzerland
+# ------------------------------------------------------------------------------
+# Name.......: adb.tf
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
+# Editor.....: Stefan Oehrli
+# Date.......: 2024.10.18
+# Revision...: 1.0.0
+# Purpose....: Define Autonomous Database (ADB) resources for the LAB environment.
+# Notes......: This resource creates Autonomous Databases (ADB) across multiple 
+#              labs using the specified configuration, such as compute count,
+#              workload type, and storage size.
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ------------------------------------------------------------------------------
+
+resource "oci_database_autonomous_database" "autonomous_database" {
+  # Create an ADB instance for each lab. The total number of ADBs is calculated
+  # by multiplying the number of labs with the number of ADB instances per lab.
+  count = var.numberOf_labs * var.adb_per_lab
+
+  # Assign each ADB to the correct lab compartment, distributed across the labs
+  compartment_id = oci_identity_compartment.lab-compartment[count.index % var.numberOf_labs].id
+
+  # Administrative password for the ADB instances
+  admin_password = local.lab_def_password
+
+  # Set the maintenance schedule type for the Autonomous Database to "REGULAR"
+  autonomous_maintenance_schedule_type = "REGULAR"
+
+  # Define the compute resources for the ADB instances
+  compute_count           = var.adb_compute_count
+  compute_model           = var.adb_compute_model
+  data_storage_size_in_gb = var.adb_data_storage_size_in_gb
+
+  # Generate a unique DB name using workload type, version, and lab index
+  db_name = upper(format("%s%s%02d", local.db_workload_type, var.adb_version, floor(count.index / var.numberOf_labs + 1)))
+
+  # Define the database version and workload type (e.g., OLTP, DW, etc.)
+  db_version  = var.adb_version
+  db_workload = var.adb_workload
+
+  # Display name format for the ADB, includes region key, environment code, and lab number
+  display_name = format("adb-%s-%s-%s-%02d-%s", lower(local.current_region_key), lower(var.environment_code), lower(local.resource_prefix_shortname), count.index % var.numberOf_labs + 1, lower(format("%s%s%02d", local.db_workload_type, var.adb_version, floor(count.index / var.numberOf_labs + 1))))
+
+  # Specify that the ADB is not dedicated, it's for development tier
+  is_dedicated = "false"
+  is_dev_tier  = var.adb_is_dev_tier
+
+  # Define other ADB settings related to security and license model
+  is_mtls_connection_required                    = "false"
+  is_preview_version_with_service_terms_accepted = "false"
+  license_model                                  = "LICENSE_INCLUDED"
+
+  # Specify the IP addresses that can access the ADB instances (whitelisting)
+  whitelisted_ips = [oci_core_vcn.vcn[count.index].id]
+}
+# --- EOF ----------------------------------------------------------------------
