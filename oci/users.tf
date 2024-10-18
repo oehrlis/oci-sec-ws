286a236403a44970d0b2a79df639f0edd3431b01
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-10-18 11:21:12 +0200

    add user file

---
+++ oci/users.tf
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# OraDBA - Oracle Database Infrastructure and Security, 5630 Muri, Switzerland
+# ------------------------------------------------------------------------------
+# Name.......: users.tf
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
+# Editor.....: Stefan Oehrli
+# Date.......: 2024.10.16
+# Revision...: 
+# Purpose....: Define IAM resources for the LAB environment.
+# Notes......: 
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ------------------------------------------------------------------------------
+
+# --- EOF ----------------------------------------------------------------------
