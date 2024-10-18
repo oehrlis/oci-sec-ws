7aa0fcbdf38cf249af8b2e92deebb9369947c3f5
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-10-18 11:05:10 +0200

    add compartment resource config

--- oci/compartment.tf
+++ oci/compartment.tf
@@ -2,26 +2,41 @@
 # OraDBA - Oracle Database Infrastructure and Security, 5630 Muri, Switzerland
 # ------------------------------------------------------------------------------
 # Name.......: compartment.tf
-# Author.....: Stefan Oehrli (oes) stefan.oehrli@accenture.com
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
 # Editor.....: Stefan Oehrli
-# Date.......: 2024.10.16
-# Revision...: 
-# Purpose....: Collect information on existing ressources for this configuration.data
-#              e.g. base linux OCID, compartment OCID, etc
-# Notes......: -- 
+# Date.......: 2024.10.18
+# Revision...: 1.0.0
+# Purpose....: Create and configure lab compartments for the LAB environment.
+#              This file also gathers existing resource information for use in
+#              the environment setup, such as compartment OCIDs.
+# Notes......: Resources like compute instances, databases, etc., will be placed 
+#              in these compartments based on the number of labs.
 # Reference..: --
 # License....: Apache License Version 2.0, January 2004 as shown
 #              at http://www.apache.org/licenses/
 # ------------------------------------------------------------------------------
 
-# create the lab compartments in home region
+# Create lab compartments in the home region. The number of compartments is based
+# on the number of labs defined in the variable 'numberOf_labs'. Each compartment
+# is uniquely named and tagged according to the lab environment settings.
 resource "oci_identity_compartment" "lab-compartment" {
-  provider       = oci.home
-  count          = var.numberOf_labs
+  provider = oci.home # Specifies the OCI provider in the home region.
+
+  # The 'count' is set by the number of labs, creating a compartment for each lab.
+  count = var.numberOf_labs
+
+  # The parent compartment ID where lab compartments will be created.
   compartment_id = var.compartment_ocid
-  description    = format("LAB Compartment for %s environment %02d (%d/%d)", upper(local.resource_name), count.index, count.index + 1, var.numberOf_labs)
-  name           = var.label_prefix == "none" ? format("%s-Lab%02d", upper(local.resource_name), count.index) : format("${var.label_prefix}-%s-Lab%02d", upper(local.resource_name), count.index)
-  freeform_tags  = var.tags
+
+  # A description of each compartment, including environment code and lab index.
+  description = format("%s Compartment for %s environment %02d (%d/%d)", upper(var.environment_code), upper(local.resource_name), count.index, count.index + 1, var.numberOf_labs)
+
+  # The name of each compartment, which is a combination of the resource name,
+  # environment code, and lab index.
+  name = format("%s-%s-%02d", upper(local.resource_name), upper(var.environment_code), count.index)
+
+  # Freeform tags for tagging resources with additional metadata.
+  freeform_tags = var.tags
 }
 
-# --- EOF -------------------------------------------------------------------
+# --- EOF ----------------------------------------------------------------------
