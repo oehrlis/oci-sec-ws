7c226249461470dee97ccdaacda26e23d206a12c
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-10-28 17:35:13 +0100

    build version 0.5.0

--- oci/main.tf
+++ oci/main.tf
@@ -5,7 +5,7 @@
 # Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
 # Editor.....: Stefan Oehrli
 # Date.......: 2024.10.16
-# Revision...: 0.4.2
+# Revision...: 0.5.0
 # Purpose....: Main configuration to build the training environment.
 # Notes......: 
 # Reference..: --
