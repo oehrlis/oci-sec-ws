8e7aa7644c03b46c3f38ea57b97eb3724b4d828a
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-11-21 16:42:10 +0100

    update release tag for 0.12.1

--- oci/logging.tf
+++ oci/logging.tf
@@ -5,7 +5,7 @@
 # Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
 # Editor.....: Stefan Oehrli
 # Date.......: 2024.10.23
-# Revision...: 0.12.0
+# Revision...: 0.12.1
 # Purpose....: Define OCI Log Group and logs for subnets in the LAB environment.
 #              This includes logs for public, private compute, and private 
 #              database subnets.
